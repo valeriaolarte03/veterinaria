12f99c4bb7f03f7df45da2883800287c5a2bf14e
valeria <valeriaolarte63@gmail.com>
2025-11-12 10:46:57 -0500

    actualizando proyecto

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->integer('rol_id');
+            $table->date('fecha_creacion');
+            $table->timestamps();
+
+            // $table->foreign('rol_id')
+            //       ->references('id')
+            //       ->on('roles')
+            //       ->onDelete('cascade');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
