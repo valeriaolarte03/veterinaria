0b6c8d9312bd6ec0b20dfb41639db7bdac6fc0c6
Jeferson Rios <jefry1795@hotmail.com>
2025-11-12 19:55:16 -0500

    Fix form action route for role creation

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-md mx-auto py-8">
         <h1 class="text-2xl font-bold mb-4">Nuevo rol</h1>
 
-        <form action="{{ route('roles.store') }}" method="POST" class="space-y-4">
+        <form action="{{ route('rol.store') }}" method="POST" class="space-y-4">
             @csrf
             <div>
                 <label class="block font-medium">Nombre</label>
